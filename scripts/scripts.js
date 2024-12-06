346a16eca409926513f2eb8f08dd10df247f0048
nathanrjackson <nathanrjackson30@gmail.com>
2024-12-05 18:56:20 -0700

    Finished HTML & CSS for projects.html and started JavaScript

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const factHeader = document.querySelector(".fact-header")
+const factDescription = document.querySelector(".fact-description")
+const factImage = document.querySelector(".fact-image")
+
+// factHeader.innerHTML = ""
+// factDescription.innerHTML = ""
+// factImage.src = ""
