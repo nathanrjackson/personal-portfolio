d9e4bebf96d4ca8cdbb6bd31f29b55d6082a9a61
nathanrjackson <nathanrjackson30@gmail.com>
2024-12-11 14:05:54 -0700

    finished v1 of website

--- scripts/projects.js
+++ scripts/projects.js
@@ -33,11 +33,15 @@ const projects = [
     project10
 ]
 
-projects.forEach(project => {
-    project.addEventListener("click", () => {
-        projectInfo.classList.remove("hide")
+if (projects) {
+    projects.forEach(project => {
+        project.addEventListener("click", () => {
+            projectInfo.classList.remove("hide")
+        })
     })
-})
+} else {
+    console.log("No projects found.")
+}
 
 closeButton.addEventListener("click", () => {
     projectInfo.classList.add("hide")
@@ -54,61 +58,61 @@ project2.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project2.title
     projectDescription.textContent = projectInformation.project2.description
     projectLanguages.textContent = projectInformation.project2.languages
-    projectLink.textContent = projectInformation.project2.link
+    projectLink.href = projectInformation.project2.link
 })
 
 project3.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project3.title
     projectDescription.textContent = projectInformation.project3.description
     projectLanguages.textContent = projectInformation.project3.languages
-    projectLink.textContent = projectInformation.project3.link
+    projectLink.href = projectInformation.project3.link
 })
 
 project4.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project4.title
     projectDescription.textContent = projectInformation.project4.description
     projectLanguages.textContent = projectInformation.project4.languages
-    projectLink.textContent = projectInformation.project4.link
+    projectLink.href = projectInformation.project4.link
 })
 
 project5.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project5.title
     projectDescription.textContent = projectInformation.project5.description
     projectLanguages.textContent = projectInformation.project5.languages
-    projectLink.textContent = projectInformation.project5.link
+    projectLink.href = projectInformation.project5.link
 })
 
 project6.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project6.title
     projectDescription.textContent = projectInformation.project6.description
     projectLanguages.textContent = projectInformation.project6.languages
-    projectLink.textContent = projectInformation.project6.link
+    projectLink.href = projectInformation.project6.link
 })
 
 project7.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project7.title
     projectDescription.textContent = projectInformation.project7.description
     projectLanguages.textContent = projectInformation.project7.languages
-    projectLink.textContent = projectInformation.project7.link
+    projectLink.href = projectInformation.project7.link
 })
 
 project8.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project8.title
     projectDescription.textContent = projectInformation.project89description
     projectLanguages.textContent = projectInformation.project8.languages
-    projectLink.textContent = projectInformation.project8.link
+    projectLink.href = projectInformation.project8.link
 })
 
 project9.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project9.title
     projectDescription.textContent = projectInformation.project9.description
     projectLanguages.textContent = projectInformation.project9.languages
-    projectLink.textContent = projectInformation.project9.link
+    projectLink.href = projectInformation.project9.link
 })
 
 project10.addEventListener("click", () => {
     projectLabel.textContent = projectInformation.project10.title
     projectDescription.textContent = projectInformation.project10.description
     projectLanguages.textContent = projectInformation.project10.languages
-    projectLink.textContent = projectInformation.project10.link
+    projectLink.href = projectInformation.project10.link
 })
